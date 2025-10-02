1a760c52b36df259b74afbbf7951d59b91cdbc7d
jihansofts <jihanuddinsoft@gmail.com>
2025-10-02 22:55:51 +0600

    update rider responsive

--- src/common/OrderCard.tsx
+++ src/common/OrderCard.tsx
@@ -17,35 +17,47 @@ export default function OrderCard({ orderData, updateStatus }: OrderCardProps) {
           className="border border-primary rounded-lg p-4 flex justify-between items-center bg-primary/5 shadow-md">
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-3">
-              <p className="text-lg font-medium font-inter">Delivering To:</p>
-              <p className="text-lg font-medium font-inter">Address:</p>
-              <p className="text-lg font-medium font-inter">Product Type:</p>
+              <p className="lg:text-lg md:text-[16px] text-[12px] font-medium font-inter">
+                Delivering To:
+              </p>
+              <p className="lg:text-lg md:text-[16px] text-[12px] font-medium font-inter">
+                Address:
+              </p>
+              <p className="lg:text-lg md:text-[16px] text-[12px] font-medium font-inter">
+                Product Type:
+              </p>
             </div>
             <div className="space-y-3">
-              <p className="text-lg font-normal">{order.deliveringTO}</p>
-              <p className="text-lg font-normal">{order.address}</p>
-              <p className="text-lg font-normal">{order.productType}</p>
+              <p className="lg:text-lg md:text-[16px] text-[12px] font-normal">
+                {order.deliveringTO}
+              </p>
+              <p className="lg:text-lg md:text-[16px] text-[12px] font-normal">
+                {order.address}
+              </p>
+              <p className="lg:text-lg md:text-[16px] text-[12px] font-normal">
+                {order.productType}
+              </p>
             </div>
           </div>
           <div className="flex flex-col justify-between space-y-5">
-            <p className="text-[32px] font-sans font-semibold text-primary">
+            <p className="lg:text-[32px] md:text-[22px] text-[18px] font-sans font-semibold text-primary">
               {order.price.toFixed(2)} CAD
             </p>
             {order.status === "Accept Order" ? (
               <button
-                className="border border-primary text-primary text-lg font-inter px-4 py-2 rounded"
+                className="border border-primary text-primary lg:text-lg md:text-[16px] text-[12px] font-inter px-4 py-2 rounded"
                 onClick={() => updateStatus(order.id, "Pending")}>
                 Accept Order
               </button>
             ) : order.status === "Pending" ? (
               <button
-                className="bg-[#FF9D13] text-white px-8 py-2 text-lg font-inter rounded"
+                className="bg-[#FF9D13] text-white px-8 py-2 lg:text-lg md:text-[16px] text-[12px] font-inter rounded"
                 onClick={() => updateStatus(order.id, "Delivered")}>
                 Pending
               </button>
             ) : (
               <button
-                className="bg-[#008136] text-white px-8 py-2 text-lg font-inter rounded"
+                className="bg-[#008136] text-white px-8 py-2 lg:text-lg md:text-[16px] text-[12px] font-inter rounded"
                 disabled>
                 Delivered
               </button>
